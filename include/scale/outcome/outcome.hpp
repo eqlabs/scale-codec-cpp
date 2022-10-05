a1324d7e3db5db67858c598019ae64e9c65b8fec
Dmytro Gordon <g-d@ukr.net>
2022-10-05 12:28:53 +0300

    Move to latest boost

--- include/scale/outcome/outcome.hpp
+++ include/scale/outcome/outcome.hpp
@@ -25,6 +25,16 @@ using result = basic_result<R, S, NoValuePolicy>;
 
 } // namespace scale::outcome
 
+// Starting from commit d33e73cf2f643f73930518cb253bf80efbca70a4 in version 1.76
+// Boost Outcome doesn't add `auto&&` to a variable automatically. So we need to do it
+// manually for backward-compatitbility.
+#if BOOST_VERSION < 107600
 #define OUTCOME_TRY(...) BOOST_OUTCOME_TRY(__VA_ARGS__)
+#else
+#define OUTCOME_TRY_1(x) BOOST_OUTCOME_TRY(x)
+#define OUTCOME_TRY_2(x, y) BOOST_OUTCOME_TRY(auto&& x, y)
+#define _GET_OUTCOME_MACRO(_1, _2, NAME, ...) NAME
+#define OUTCOME_TRY(... ) _GET_OUTCOME_MACRO(__VA_ARGS__, OUTCOME_TRY_2, OUTCOME_TRY_1)(__VA_ARGS__)
+#endif // BOOST_VERSION < 107600
 
 #endif // SCALE_OUTCOME_OUTCOME
